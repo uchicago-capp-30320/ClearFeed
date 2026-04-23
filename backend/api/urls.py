35acc5052b042ac5896ed253fafc0c351f36668c
Yuri Chang <yurichang@uchicago.edu>
2026-04-22 23:02:18 -0500

    ml pipeline building: session_id to get tweet text, one sentiment model

--- backend/api/urls.py
+++ backend/api/urls.py
@@ -3,6 +3,7 @@ from . import views
 
 urlpatterns = [
     path('import-dataset/', views.import_dataset, name='import_dataset'),
+    path('analyze-sentiment/', views.analyze_sentiment, name='analyze_sentiment'),
 ]
 
 # if the URL is import-dataset/ run the import_dataset function from views.py.
