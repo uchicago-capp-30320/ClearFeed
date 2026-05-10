e74206b68b1159d5e2ba58f7e920af47ff2f40aa
grace-kluender <graceek@uchicago.edu>
2026-05-10 12:34:49 -0500

    fixed linting from backend queue commit

--- backend/clearfeed_django/settings.py
+++ backend/clearfeed_django/settings.py
@@ -45,12 +45,11 @@ INSTALLED_APPS = [
     "django.contrib.messages",
     "django.contrib.staticfiles",
     "rest_framework",  # adding Django REST framework to help with building endpoints
-    "corsheaders",  # adding CORS library to allow browser extension to make requests to Django server
     "api",  # adding app so Django recognizes everything in that folder
+    "django_q",
 ]
 
 MIDDLEWARE = [
-    "corsheaders.middleware.CorsMiddleware",  # activates CORS protection for Django server
     "django.middleware.security.SecurityMiddleware",
     "django.contrib.sessions.middleware.SessionMiddleware",
     "django.middleware.common.CommonMiddleware",
@@ -59,6 +58,14 @@ MIDDLEWARE = [
     "django.contrib.messages.middleware.MessageMiddleware",
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
 ]
+
+Q_CLUSTER = {
+    "name": "clearfeed",
+    "workers": 2,  # allows for parallel processing, but still small?
+    "timeout": 1200,  # 20 minutes
+    "retry": 1500,  # must be larger than timeout
+    "orm": "default",
+}
 
 ROOT_URLCONF = "clearfeed_django.urls"
 
@@ -123,12 +130,6 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
 STATIC_URL = "static/"
-
-CORS_ALLOW_CREDENTIALS = True
-CORS_ALLOWED_ORIGINS = [
-    "http://localhost:8000",
-    "moz-extension://cfeeb442-cce1-4e03-9099-05a1710fa96f",
-]
 
 
 # Auth-related direction
