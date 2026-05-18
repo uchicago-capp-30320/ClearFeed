d9bb4648ab7c427cda363c5b58d2fa18bf7724df
Khushi Desai <khushi@uchicago.edu>
2026-05-18 17:21:44 -0500

    making sure the logo loafs

--- backend/clearfeed_django/settings.py
+++ backend/clearfeed_django/settings.py
@@ -164,7 +164,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
 STATIC_URL = "static/"
-
+STATICFILES_DIRS = [
+    BASE_DIR.parent / "static",
+]
 
 # Auth-related direction
 
