bbcc12343d83ffb57dfe89896b086a7a3451c9b7
grace-kluender <graceek@uchicago.edu>
2026-05-19 08:51:53 -0500

    Fixing yellow font and logo

--- backend/clearfeed_django/settings.py
+++ backend/clearfeed_django/settings.py
@@ -58,7 +58,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent  # ClearFeed/backend/
 SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ["*"]  # ALLOWING ALL ONLY FOR LOCAL DEV TESTING!!
 
